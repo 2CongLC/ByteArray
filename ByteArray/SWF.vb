722386db92eafc5473bc3e3d3f0c8d687bd888a1
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-05-12 20:02:31 +0700

    Update SWF.vb

--- ByteArray/SWF.vb
+++ ByteArray/SWF.vb
@@ -5,16 +5,38 @@ Imports System.Text
 
 Public Class SWF
 
-Private signature as String
-Private version as String
-Private fileSize as Integer
+
+Private source as ByteArray
+Private _signature as string
+Private _version as integer
+Private _fileSize as integer
   
 
-Public Sub New()
+Public Sub New(Byval buffer as Byte())
+    source = New ByteArray(buffer)
+    
+    Dim header as ByteArray = New ByteArray()
+    header.WriteBytes(source,0,3) ' Position = 0, Byte = 0, 1, 2
+    _signature = header.toString()
+
+    Dim version as ByteArray = New ByteArray()
+    version.WriteBytes(source,3,1) ' Position = 3, Byte = 3
+    _version = version.toString()
+
+    
+
     
 End Sub
 
+Public Property signature as String
+    Set (value as String)
+      _signature = value
+     End Set
+    Get
+      Return _signature
+    End Get  
+End Property
 
 
-
+  
 End Class  
