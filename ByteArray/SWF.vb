59c066d87648350c9a9d7557152f86b5a259485b
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-05-12 18:20:54 +0700

    Update SWF.vb

--- ByteArray/SWF.vb
+++ ByteArray/SWF.vb
@@ -1,1 +1,20 @@
+Imports System
+Imports System.IO
+Imports System.Text
 
+
+Public Class SWF
+
+Private signature as String
+Private version as String
+Private fileSize as Integer
+  
+
+Public Sub New()
+    
+End Sub
+
+
+
+
+End Class  
