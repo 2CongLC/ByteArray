16b41c33f9d4e2df05b820d95cce164460f9fb20
ChuCong <2conglc.vn@gmail.com>
2024-05-15 09:05:20 +0700

    update

--- ByteArray/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Try
-
-        Catch ex As Exception
-
-        End Try
-    End Sub
-End Class
